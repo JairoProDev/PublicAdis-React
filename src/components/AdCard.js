c6336e7926b91e8cdd16de7048af8af2a9293744
Jairo Saul <JairoProDev@gmail.com>
2024-01-15 12:02:04 -0500

    Add AdCard and Sidebar components

---
+++ src/components/AdCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AdCard({ anuncio }) {
+    return (
+        <li className="anuncio-card">
+            <h3 className="title">{anuncio.title || anuncio.titulo || anuncio.adTitle}</h3>
+            <p className="description">{anuncio.description || anuncio.descripcion || anuncio.adDescription}</p>
+        </li>
+    );
+}
+
+export default AdCard;
