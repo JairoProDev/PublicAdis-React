afe0b6d32dfcb7c672dea1c74398b276b9a2d2e3
Jairo Saul <JairoProDev@gmail.com>
2024-01-17 13:52:16 -0500

    Update header design and add new icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,24 +29,53 @@ function Header() {
         <header className={`header ${isHidden ? 'header-hidden' : ''}`}>
             <div className="header-left">
                 <img src={logo} alt="Logo" className="logo" />
-                <div className="header-title">PublicAdis: AnuncIA</div>
+                <div className="header-title">PublicAdis</div>
                 <div className="search-container">
                     <input type="text" placeholder="Buscar en PublicAdis" className="search-input" />
                 </div>
             </div>
             <nav className="nav">
                 <ul className="nav-list">
-                    <li className="nav-item"><a href="/Empleos">Empleos</a></li>
-                    <li className="nav-item"><a href="/Inmuebles">Inmuebles</a></li>
-                    <li className="nav-item"><a href="/Autos">Autos</a></li>
-                    <li className="nav-item"><a href="/Servicios">Servicios</a></li>
+                    <li className="nav-item">
+                    <span className="material-symbols-outlined">work</span>
+                        <a href="/Empleos">Empleos</a>
+                    </li>
+                    <li className="nav-item">
+                    <span className="material-symbols-outlined">house</span>
+                        <a href="/Inmuebles">Inmuebles</a>
+                    </li>
+                    <li className="nav-item">
+                    <span className="material-symbols-outlined">directions_car</span>
+                        <a href="/Autos">Autos</a>
+                    </li>
+                    <li className="nav-item">
+                    <span className="material-symbols-outlined">engineering</span>
+                        <a href="/Servicios">Servicios</a>
+                        </li>
                 </ul>
             </nav>
             <div className="header-right">
+            <div className="nav-item">
+                <span className="material-symbols-outlined">account_circle</span>
                 <a href="/perfil">Mi Perfil</a>
-                <a href="/configuracion">Configuración</a>
-                <a href="/anunciar">AnuncIAr</a>
             </div>
+            <div className="nav-item">
+                <span className="material-symbols-outlined">notifications_none</span>
+                <a href="/notificaciones">Notificaciones</a>
+            </div>
+            <div className="nav-item">
+                <span className="material-symbols-outlined">message</span>
+                <a href="/mensajes">Mensajes</a>
+            </div>
+            <div className="nav-item">
+                <span className="material-symbols-outlined">settings</span>
+                <a href="/configuracion">Configuración</a>
+            </div>
+            <div className="nav-item">
+                <span className="material-symbols-outlined">campaign</span>
+                <a href="/anunciar">Anunciar</a>
+            </div>
+        </div>
         </header>
         
     );
