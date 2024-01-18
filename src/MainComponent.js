47b676912d235b767abc71f50dc78a541cc470e3
Jairo Saul <JairoProDev@gmail.com>
2024-01-18 12:32:34 -0500

    Refactor styles and components

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,45 +1,43 @@
+import "./styles/root.css";
+import "./styles/reset.css";
+import "./styles/body.css";
 
-import './styles/root.css';
-import './styles/reset.css';
-import './styles/body.css';
+import "./MainComponent.css";
 
-import './MainComponent.css';
+import "./styles/navigation.css";
+import "./styles/form.css";
+import "./styles/responsive.css";
+import "./styles/editForm.css";
+import "./styles/navbar.css";
+import "./styles/mainContent.css";
+import "./styles/PublishButton.css";
+import "./styles/adCard.css";
 
-import './styles/navigation.css';
-import './styles/form.css';
-import './styles/responsive.css';
-import './styles/editForm.css';
-import './styles/navbar.css';
-import './styles/mainContent.css';
-import './styles/PublishButton.css';
-// import './styles/adsList.css';
-import './styles/adCard.css';
-
-import React from 'react';
-import AdList from './components/AdList';
-import Sidebar from './components/Sidebar/Sidebar';
-import AdForm from './components/AdForm/AdForm';
+import React, { Fragment } from "react";
+import AdList from "./components/AdList/AdList";
+import Sidebar from "./components/Sidebar/Sidebar";
+import AdForm from "./components/AdForm/AdForm";
 // import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import Home from './pages/Home';
-import Header from './components/Header/Header';
-import useAds from './hooks/useAds';
+import Header from "./components/Header/Header";
+import useAds from "./hooks/useAds";
 
 function MainComponent() {
-    const { anuncios, agregarAnuncioAlPrincipio, error } = useAds();
+  const { anuncios, agregarAnuncioAlPrincipio, error } = useAds();
 
-    return (
-        <div>
-            <Header/>
-            <div className="container">
-                <div className="main-content">
-                    <Sidebar/>
-                    <AdList anuncios={anuncios} />
-                    <AdForm agregarAnuncioAlPrincipio={agregarAnuncioAlPrincipio} />
-                    {error && <div className="error">{error}</div>}
-                </div>
-            </div>
+  return (
+    <Fragment>
+      <Header />
+      <div className="container">
+        <div className="main-content">
+          <Sidebar />
+          <AdList anuncios={anuncios} />
+          <AdForm agregarAnuncioAlPrincipio={agregarAnuncioAlPrincipio} />
+          {error && <div className="error">{error}</div>}
         </div>
-    );
+      </div>
+    </Fragment>
+  );
 }
 export default MainComponent;
